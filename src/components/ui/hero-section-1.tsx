d0fba276785ef8576868e2430dfed02e57fb05e6
KauTryKauCuba <muddmm91.learn@gmail.com>
2026-04-19 13:27:55 +0800

    feat: removed customer logo section from hero

--- src/components/ui/hero-section-1.tsx
+++ src/components/ui/hero-section-1.tsx
@@ -35,7 +35,7 @@ export function HeroSection() {
                                         </div>
                                     </div>
                                 </Link>
-                    
+
                                 <h1
                                     className="mt-8 max-w-4xl mx-auto text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem]">
                                     Modern Solutions for Customer Engagement
