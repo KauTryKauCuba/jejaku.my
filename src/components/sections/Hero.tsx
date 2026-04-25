ccf1cc7ca49e889dbeee4e18b71cc1a4c9d41e1d
KauTryKauCuba <muddmm91.learn@gmail.com>
2026-04-25 15:56:56 +0800

    style: make hero title bold

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -32,7 +32,7 @@ export const Hero = () => {
               </div>
             </Link>
 
-            <h1 className="mt-6 max-w-4xl mx-auto text-balance text-5xl md:text-6xl lg:mt-8 xl:text-[4.5rem]">
+            <h1 className="mt-6 max-w-4xl mx-auto text-balance text-5xl font-bold md:text-6xl lg:mt-8 xl:text-[4.5rem]">
               Modern Solutions for Customer Engagement
             </h1>
             <p className="mx-auto mt-6 max-w-2xl text-balance text-lg">
